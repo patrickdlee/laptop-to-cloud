34e28c16f937a1a30e14ed100845c66c312bf64f
Patrick Lee <patrick.lee@bodybuilding.com>
2014-12-18 17:01:41 -0700

    added initial example Web page

---
+++ www/example/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>From Your Laptop to the Cloud</title>
+</head>
+<body>
+  <h1>From Your Laptop to the Cloud</h1>
+  <p>Useful stuff will go here.</p>
+</body>
+</html>
